d40368ebe1c383672297dc27523839b4a9e309f2
majuss8 <mjuliamary0@gmail.com>
2024-06-13 16:44:40 -0300

    commit inicial

--- src/index.js
+++ src/index.js
@@ -3,15 +3,31 @@
 // }) // pode criar um arquivo 
 require('dotenv').config();
 const cors = require('cors');
-
+const knex = require('./conexao');
 const express = require('express');
 
 const app = express();
+
 app.use(cors())
+
+// app.use(cors({
+//     origin: 'https://cubos.academy//' //domínio da cubos 
+// }))
+
+// app.use(cors({
+//     origin: ['https://cubos.academy//', 'https://google.com/'] // array de domínios que terão acesso a sua api
+// }))
+
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    return res.json('API está OK!')
+    try {
+        const carros = await knex('carros')
+        return res.json(carros)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ mensagem: 'Erro do servidor' })
+    }
 });
 
 const port = process.env.PORT || 3000
